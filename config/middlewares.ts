2c1e72dac6497246eee125a21e31981c33202727
coderghoust <tabish01099@gmail.com>
2025-06-17 06:50:08 -1200

    updated config and middleware file

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,7 +1,46 @@
 export default [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'https://market-assets.strapi.io',
+            'https://res.cloudinary.com',
+            'https://console.cloudinary.com',
+          ],
+          'script-src': [
+            "'self'",
+            "'unsafe-inline'",
+            'https://media-library.cloudinary.com',
+            'https://upload-widget.cloudinary.com',
+            'https://console.cloudinary.com',
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'https://res.cloudinary.com',
+            'https://console.cloudinary.com',
+          ],
+          'frame-src': [
+            "'self'",
+            'https://media-library.cloudinary.com',
+            'https://upload-widget.cloudinary.com',
+            'https://console.cloudinary.com',
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,10 @@
-export default () => ({});
+export default () => ({
+  'cloudinary-media-library': {
+    enabled: true,
+    config: {
+      cloudName: process.env.CLOUDINARY_NAME,
+      apiKey: process.env.CLOUDINARY_API_KEY,
+      encryptionKey: process.env.CLOUDINARY_ENCRYPTION_KEY,
+    },
+  },
+});
